f8e1b3ef815ac1e108f34fb108e833d20d27f737
Juan <juandgoc@gmail.com>
2025-05-01 14:58:58 -0500

    material web with sidepanel example

---
+++ src/sidepanel.ts
@@ -0,0 +1,5 @@
+
+import '@material/web/all.js';
+import {styles as typescaleStyles} from '@material/web/typography/md-typescale-styles.js';
+
+document.adoptedStyleSheets.push(typescaleStyles.styleSheet!);
